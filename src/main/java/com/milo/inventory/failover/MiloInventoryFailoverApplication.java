5e52608f72256d63164c9c61d60f5ad64a9b7679
Camilo Romero <militoromero10@gmail.com>
2021-07-10 11:14:25 -0500

    controller to generate response for handling error inventory

---
+++ src/main/java/com/milo/inventory/failover/MiloInventoryFailoverApplication.java
@@ -0,0 +1,13 @@
+package com.milo.inventory.failover;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiloInventoryFailoverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiloInventoryFailoverApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/milo/inventory/failover/MiloInventoryFailoverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.milo.inventory.failover;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiloInventoryFailoverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
